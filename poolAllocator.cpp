35576461f27b236ad8614791d57fbfe6f92b82e4
Astronova1 <awaiz123nasir123@gmail.com>
2026-05-20 13:21:01 +0500

    Initial commit

---
+++ poolAllocator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dark on 20/05/2026.
+//
+
+#include "poolAllocator.h"
---
+++ poolAllocator.h
@@ -0,0 +1,13 @@
+//
+// Created by Dark on 20/05/2026.
+//
+
+#ifndef POOL_ALLOCATOR_POOLALLOCATOR_H
+#define POOL_ALLOCATOR_POOLALLOCATOR_H
+
+
+class poolAllocator {
+};
+
+
+#endif //POOL_ALLOCATOR_POOLALLOCATOR_H
