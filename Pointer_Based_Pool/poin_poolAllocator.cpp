19dcc095803ce14a29d275bc7c64b79d16e131b3
Astronova1 <awaiz123nasir123@gmail.com>
2026-06-05 20:23:49 +0500

    fixed the incorrect linkage of the list add function to travers the list

--- Pointer_Based_Pool/poin_poolAllocator.cpp
+++ Pointer_Based_Pool/poin_poolAllocator.cpp
@@ -11,34 +11,28 @@ poin_poolAllocator::poin_poolAllocator( const int slot_size)
     : m_head {nullptr}
     , m_slot_size {slot_size}
 {
-    m_arena = new std::byte[m_slot_size*sizeof(int)];
+    m_arena = new std::byte[m_slot_size* 8];
     for (int i=0; i<slot_size; i++) {
         std::byte* current = m_arena + slot_size * i;     //current pointer of 8 bytes
-        std::byte* p_next = m_arena + slot_size * (i+2);  //next pointer of 8 bytes
+        std::byte* p_next = m_arena + slot_size * (i+1);  //next pointer of 8 bytes
         std::byte** next;                                // pointer to a pointer
         next = reinterpret_cast<std::byte**>(current);  // imagine current(i.e byte next)  as pointer to pointer
-        if (i < m_slot_size-1) {
+        if (i == m_slot_size-1) {
             *next = nullptr;                           //assign null pointer to the last
         }else {
             *next = p_next;                             // assign next pointer (p_next) to current pointer
         }
-        do {
-
-            std::cout << "Slot: "<< i << " " << current << "\n" <<"Slot: " << i+1 << " " << p_next << std::endl;
-            i+= 1;
-        } while (i >= 8);
     }
     m_head = m_arena ;
-    std::cout << "Head is " << m_head << std::endl;
 }
 
- std::optional<std::byte*> poin_poolAllocator::allocate() {             //allocate the free memory space
+ std::optional<std::byte**> poin_poolAllocator::allocate() {             //allocate the free memory space
      if (m_head == nullptr) {         //return -1 if memory arena is empty
          std::cout << "All memory spaces filled" << std::endl;
          return std::nullopt;
      }
-    std::byte* allo = m_head;
-    m_head = m_arena + m_slot_size;
+    std::byte** allo = reinterpret_cast<std::byte**>(m_head);
+    m_head = *allo;
                 //allocate the address inside the current head as the new head
      std::cout << std::endl << allo << " allocated" << std::endl;
      std::cout << "New head is : " << m_head << std::endl;
@@ -56,22 +50,23 @@ poin_poolAllocator::poin_poolAllocator( const int slot_size)
 //     return 0;
 // }
 //
-// int poin_poolAllocator::printlist() const {         //travers the free list if it is not allocated
-//     int curr = m_head;
-//     if (curr == -1) {               //return -1 if no memory slot free
-//         std::cout << "All slot are Allocated";
-//         return -1;
-//     }
-//     std::cout << std::endl;
-//     std::cout << "======FREE_LIST======" << std::endl;
-//     while ( curr != -1 ) {              // check if there are free slots and run the loops until the conditions becomes false
-//         std::cout << std::endl << curr << " Points to " << m_add[curr] << std::endl ;    //the current head value pointing to the next slot
-//         curr = m_add[curr];   //change the current to the next slot value
-//     }
-//     std::cout << std::endl << std::endl;
-//     return 0;
-// }
-//
+void poin_poolAllocator::printlist() const {         //travers the free list if it is not allocated
+    std::byte* curr = m_head;
+    if (curr == nullptr) {               //return nullptr if no memory slot free
+        std::cout << "All slot are Allocated";
+    }
+    std::cout << std::endl;
+    std::cout << "======FREE_LIST======" << std::endl;
+    while ( curr != nullptr ) {              // check if there are free slots and run the loops until the conditions becomes false
+        std::cout << std::endl << "Current Node Address: " << curr << std::endl;
+        // pointer to a pointer
+        std::byte **storage = reinterpret_cast<std::byte **>(curr);  // imagine current(i.e byte next)  as pointer to pointer
+        curr = *storage;
+        std::cout << "Points To: " << curr << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+}
+
 // poin_poolAllocator::~poin_poolAllocator() {
 //     delete[] m_arena;
 //     std::cout << "Memory released\n";
