440e7903ac917a4cfa2261172d50d2ad273f1d12
Astronova1 <awaiz123nasir123@gmail.com>
2026-06-10 20:56:45 +0500

    allow user defined slot size

--- Pointer_Based_Pool/pointerbased.cpp
+++ Pointer_Based_Pool/pointerbased.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 
 int main() {
-    poin_poolAllocator p(8);
+    poin_poolAllocator p(8,4);
+    p.printlist();
     std::optional<std::byte*> alloc_result = p.allocate();
     std::byte* ptr = nullptr;
     if (alloc_result.has_value()) {
         ptr = alloc_result.value();
     }
+    p.printlist();
     p.allocate();
     p.printlist();
     p.de_allocate(ptr);
